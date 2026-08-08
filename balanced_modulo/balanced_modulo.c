86f8f75d4193a8246dece67332fbf0a6d83211eb
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-08-08 10:35:39 -0400

    Add balanced_divide() function to balanced_modulo.c

--- balanced_modulo/balanced_modulo.c
+++ balanced_modulo/balanced_modulo.c
@@ -4,8 +4,7 @@ int true_modulo(int n, unsigned int d)
 {
     int signed_d = d;
     int remainder = n % signed_d;
-    if (remainder < 0)
-    {
+    if (remainder < 0) {
         return remainder + signed_d;
     }
     return remainder;
@@ -18,14 +17,24 @@ int balanced_modulo(int n, unsigned int d)
     return true_modulo(n + half_range, d) - half_range;
 }
 
+int balanced_divide(int n, unsigned int d)
+{
+    int m = balanced_modulo(n, d);
+    int signed_d = d;
+    return (n - m) / signed_d;
+}
+
 int main()
 {
     const unsigned int d = 5;
 
     // Test
-    for (int n = -10; n <= +10; n++)
-    {
+    for (int n = -10; n <= +10; n++) {
         printf("%d %%%% %u = %d\n", n, d, balanced_modulo(n, d));
+        printf("%d // %u = %d\n", n, d, balanced_divide(n, d));
+        if (n != 10) {
+            printf("\n");
+        }
     }
 
     return 0;
